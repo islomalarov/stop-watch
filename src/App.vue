be19d6500e3cd21c1cd3973aa243a29e9cc9e5f9
Islom Alarov <islomalarov@gmail.com>
2023-03-25 17:39:24 +0500

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="stopwatchs">
-    <StopWatch></StopWatch>
-    <StopWatch></StopWatch>
-    <StopWatch></StopWatch>
-    <StopWatch></StopWatch>
+    <StopWatch v-for="block in blocks" :key="block"></StopWatch>
+    <button class="add__block" v-on:click="addBlocks">+</button>
   </div>
 </template>
 
 <script>
 import StopWatch from './components/StopWatch.vue';
-
+import { ref } from 'vue';
 export default {
   name: 'App',
   components: {
     StopWatch
+  },
+  setup() {
+    let blocks = ref(3);
+
+    function addBlocks() {
+      blocks.value++;
+    }
+    return {
+      blocks,
+      addBlocks
+    }
   }
 
 }
@@ -35,6 +44,16 @@ export default {
   display: grid;
   justify-items: center;
   gap: 50px;
+}
+
+.add__block {
+  width: 225px;
+  height: 120px;
+  background: #696969;
+  border: none;
+  font-size: 40px;
+  color: #9E9E9E;
+  cursor: pointer;
 }
 
 @media screen and (min-width: 320px) {
--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="stopwatch__block">
         <div class="stopwatch__block-info">
-            <span class="hours">{{ hours }}</span> : <span class="hours">{{ minuts }}</span> : <span class="hours">{{ seconds }}</span>
+            <span :class="isHours ? 'hours active' : 'hours'" v-if="isHours">{{ hours > 9 ? hours : '0' + hours }} :
+            </span>
+            <span :class="isMinuts ? 'minuts active' : 'minuts'" v-if="isMinuts">{{ minuts > 9 ? minuts : '0' + minuts }}
+                : </span>
+            <span :class="isSeconds ? 'seconds active' : 'seconds'">{{ seconds > 9 ? seconds : '0' + seconds }}</span>
         </div>
         <div class="stopwatch__block-control">
             <button class="stopwatch__block-btn" v-if="isPlay" v-on:click="play"><img src="../assets/icons/треугольник.png"
-                    alt=""></button>
+                    alt="icon-1"></button>
             <button class="stopwatch__block-btn" v-if="isPaused" v-on:click="pause"> <img src="../assets/icons/Пауза.png"
-                    alt=""></button>
-            <button class="stopwatch__block-btn"> <img src="../assets/icons/квадрат.png" alt=""></button>
+                    alt="icon-2"></button>
+            <button class="stopwatch__block-btn" v-on:click="reset"> <img src="../assets/icons/квадрат.png"
+                    alt="icon-3"></button>
         </div>
     </div>
 </template>
@@ -20,34 +25,54 @@ export default {
     setup() {
         let hours = ref(0);
         let minuts = ref(0);
-        let seconds = ref(0)
+        let seconds = ref(0);
+        let isHours = ref(false);
+        let isMinuts = ref(false);
+        let isSeconds = ref(false);
         let isPlay = ref(true);
         let isPaused = ref(false);
         let interval;
 
-        const startTimer = () => {
+        function startTimer() {
             seconds.value++;
 
-            if(seconds.value > 60) {
+            if (seconds.value > 60) {
                 minuts.value++;
-                seconds.value = 0
+                isMinuts.value = true;
+                seconds.value = 0;
             }
-            if(minuts.value> 60) {
+            if (minuts.value > 60) {
+                isHours.value = true;
                 hours.value++;
-                minuts.value = 0
+                minuts.value = 0;
             }
         }
 
 
         function play() {
             isPlay.value = false;
-            isPaused.value = true
-            interval = setInterval(startTimer, 10)
+            isPaused.value = true;
+            isSeconds.value = true;
+            clearInterval(interval);
+            interval = setInterval(startTimer, 1);
         }
         function pause() {
             clearInterval(interval)
             isPaused.value = false;
             isPlay.value = true;
+
+        }
+        function reset() {
+            clearInterval(interval);
+            isPaused.value = false;
+            isPlay.value = true;
+            isHours.value = false;
+            isMinuts.value = false;
+            isSeconds.value = false;
+            hours.value = 0;
+            minuts.value = 0;
+            seconds.value = 0;
+
         }
         return {
             hours,
@@ -55,8 +80,12 @@ export default {
             seconds,
             isPlay,
             isPaused,
+            isHours,
+            isMinuts,
+            isSeconds,
             play,
             pause,
+            reset
         }
     }
 }
@@ -71,9 +100,22 @@ export default {
 }
 
 .stopwatch__block-info {
-    height: 60px;
     border-bottom: 1px solid #9E9E9E;
-    ;
+    color: #9E9E9E;
+    padding: 22px 0 20px 0;
+}
+
+span {
+    font-weight: 400;
+    font-size: 22px;
+    line-height: 21px;
+    text-align: center;
+}
+
+.minuts.active,
+.seconds.active,
+.hours.active {
+    color: white;
 }
 
 .stopwatch__block-control {
